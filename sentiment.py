948adba32266c9df50f36262d46ad7c168d7a8d1
Arman Rafiee <aran.rafiee@gmail.com>
2024-08-16 00:05:48 -0400

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,6 @@
-from transformers import pipeline
 import logging
+import numpy as np
+from transformers import pipeline
 import config
 
 class AdvancedSentimentAnalyzer:
@@ -9,25 +10,27 @@ class AdvancedSentimentAnalyzer:
         self.sentiment_model = pipeline(
             "sentiment-analysis", 
             model=config.SETTINGS['sentiment_model_name'], 
-            device=device, # (macOS) device = -1 # Force CPU usage
-            clean_up_tokenization_spaces=True
+            device=device # (macOS device = -1 # Force CPU usage
         )
 
     def analyze_sentiment(self, texts):
         logging.info("Analyzing sentiment using advanced NLP model")
         max_length = config.SETTINGS['sentiment_max_length']
-        truncated_texts = [text[:max_length] for text in texts]
+        truncated_texts = [text[:max_length] for text in texts]  # Truncate texts if too long
 
         results = self.sentiment_model(truncated_texts)
+        
+        # Convert the results into a simple score (positive or negative)
         scores = []
         for result in results:
             score = result['score'] if result['label'] == 'POSITIVE' else -result['score']
             scores.append(score)
 
+        # Calculate the weighted average sentiment score
         weighted_average_score = sum(scores) / len(scores) if scores else 0
         weighted_avg_stds = np.sum(np.std(scores)) / np.size(scores) if scores else 0
         logging.info(f"Average sentiment score: {weighted_average_score}")
         logging.info(f"Average sentiment std: {weighted_avg_stds}")
-
+        
         reliability = max(0, 100 - (weighted_avg_stds * 100))
         return weighted_average_score, reliability
